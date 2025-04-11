202fdabd7970b220e71d10dd9c85237d69cf6ecd
Arun-101112 <gmarun1021@gmail.com>
2025-04-11 23:21:48 +0530

    Updated
    Dashboard page Ui and Layout updated...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import "./App.css";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Orders from "./pages/Orders/Order";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
